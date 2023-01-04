60a4f16bb4c98302eb0910897ae5f24eb7415a60
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-04 23:03:04 +0300

    fixed

--- main/settings/base.py
+++ main/settings/base.py
@@ -203,3 +203,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+REST_AUTH_SERIALIZERS = {
+    'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
+    
+}
