69f6606f5f24d3a96a5a896566ad6124ce7e34d0
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-05 21:37:06 +0300

    permission user

--- flight/views.py
+++ flight/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import FlightSerializer
 from .models import Flight
+from rest_framework.permissions import IsAdminUser
+from .permissions import IsStafforReadOnly
 # Create your views here.
 
 class FligtView(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+    permission_classes = (IsStafforReadOnly,)
+
